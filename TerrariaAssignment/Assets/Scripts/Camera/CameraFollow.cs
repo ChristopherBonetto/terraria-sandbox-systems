14ee7fdf4c057a5c37905179d7e2750ff6ff1d87
AlessandroPantano <alessandro.pantano@outlook.it>
2019-11-03 12:55:11 +0100

    Init project. Add CameraFollow.cs

---
+++ TerrariaAssignment/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform m_Target;
+
+    [SerializeField] private bool m_IsSmooth;
+
+    [SerializeField] private float m_MovementSpeed;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var newPosition = new Vector3(m_Target.transform.position.x, m_Target.transform.position.y, transform.position.z);
+
+        if (m_IsSmooth)
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, m_MovementSpeed * Time.deltaTime);
+
+        else
+            transform.position = newPosition;
+    }
+}
