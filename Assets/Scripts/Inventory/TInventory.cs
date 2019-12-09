c85ad4ebf124001fb7e62774ddaf00204554b7cf
Christopher.Bonetto <christopherbonetto@gmail.com>
2019-12-09 11:22:16 +0100

    changed comments

--- Assets/Scripts/Inventory/TInventory.cs
+++ Assets/Scripts/Inventory/TInventory.cs
@@ -6,13 +6,16 @@ public class TInventory : MonoBehaviour
 {
     private TPlayerController m_myPlayer;
 
+    //List or items that the player will equip to the start of the game.
     [SerializeField] private List<TItemQuantity> StartingItems = new List<TItemQuantity>();
+
+    //List that it contains the reference for each TInventorySlot.
     public List<TInventorySlot> InventorySlots { get; private set; } = new List<TInventorySlot>();
 
     public bool m_isInventoryOpen { get; private set; }
 
 
-
+    #region Events
 
     private void OnEnable()
     {
@@ -23,9 +26,7 @@ public class TInventory : MonoBehaviour
         TEventManager.UnsubscribeFrom<bool>(TEventID.OnInventoryOpen, ChangeOpenCloseInventoryBool);
     }
 
-
-
-
+    #endregion
 
 
     private void Awake()
@@ -33,12 +34,17 @@ public class TInventory : MonoBehaviour
         m_myPlayer = gameObject.GetComponent<TPlayerController>();
     }
 
+    #region Collect starting items
+
     private void Start()
     {
         StartCoroutine(CollectStartingItems());
     }
 
-
+    /// <summary>
+    /// Coroutine that it wait the end of the current frame to collect all the starting items.
+    /// </summary>
+    /// <returns></returns>
     public IEnumerator CollectStartingItems()
     {
         yield return new WaitForEndOfFrame();
@@ -52,7 +58,15 @@ public class TInventory : MonoBehaviour
         }
     }
 
+    #endregion
 
+    #region Create Inventory
+
+    /// <summary>
+    /// This method copy a list by tag from the item pooler.
+    /// For each element in list take his <param TInventorySlot> component and add it to <param InventorySlots> list.
+    /// After that trigger an event to tell to all subscribed class that it is the main inventory class.
+    /// </summary>
     public void InventorySlotsReference()
     {
         List<GameObject> tempInventorySlotsList = new List<GameObject>();
@@ -81,19 +95,21 @@ public class TInventory : MonoBehaviour
         }
     }
 
-    //Used to open and close Inventory
-    public void ChangeOpenCloseInventoryBool(bool inIsOpen)
-    {
-        m_isInventoryOpen = inIsOpen;
-    }
 
     public void GiveReferenceToThisInventory()
     {
         TEventManager.TriggerEvent<TInventory>(TEventID.OnInventoryCreated, this);
     }
 
+    #endregion
 
     #region Collet New Item
+
+    /// <summary>
+    /// This method does two checks.
+    /// One check if the inventory contains an item equal to the input item. if return true increase the amount of that item.
+    /// If the first check return false so check the first empty slot and fill it with the input item.
+    /// </summary>
     public void CollectItem(TItemQuantity inAddThisItem)
     {
         if (!CheckSimilarItemsAndAddValue(inAddThisItem))
@@ -135,31 +151,14 @@ public class TInventory : MonoBehaviour
             }
         }
     }
+
     #endregion
 
-    public TItemQuantity ChangeSlotValue(TItemQuantity inSlot, int inValue)
-    {
-        TItemQuantity tempItem = new TItemQuantity(inSlot.Item, inSlot.Amount);
-        tempItem.Amount += inValue;
-        return tempItem;
-    }
-    
+    #region Decrease amount to an slotted item.
 
-    public List<TItemQuantity> ItemsInInventory()
-    {
-        List<TItemQuantity> tempItemsList = new List<TItemQuantity>();
-
-        for(int i = 0; i < InventorySlots.Count; i++)
-        {
-            if (InventorySlots[i].ItemInSlot != TItemQuantity.Empty)
-            {
-                tempItemsList.Add(InventorySlots[i].ItemInSlot);
-            }
-        }
-        return tempItemsList;
-    }
-
-
+    /// <summary>
+    /// Used to check if exist in list the input's item. If return true decreases his amount.
+    /// </summary>
     public bool CheckSimilarItemsAndRemoveValue(TItemQuantity inAddThisItem)
     {
         for (int i = 0; i < InventorySlots.Count; i++)
@@ -180,4 +179,43 @@ public class TInventory : MonoBehaviour
         }
         return false;
     }
+
+    #endregion
+
+    #region Methods to take/change inventory infos.
+
+    //Triggered from event change the bool of this inventory.
+    public void ChangeOpenCloseInventoryBool(bool inIsOpen)
+    {
+        m_isInventoryOpen = inIsOpen;
+    }
+
+    /// <summary>
+    /// Used to change the amount of an item.
+    /// </summary>
+    public TItemQuantity ChangeSlotValue(TItemQuantity inSlot, int inValue)
+    {
+        TItemQuantity tempItem = new TItemQuantity(inSlot.Item, inSlot.Amount);
+        tempItem.Amount += inValue;
+        return tempItem;
+    }
+
+    /// <summary>
+    /// Returns a list of all items in inventory.
+    /// </summary>
+    public List<TItemQuantity> ItemsInInventory()
+    {
+        List<TItemQuantity> tempItemsList = new List<TItemQuantity>();
+
+        for (int i = 0; i < InventorySlots.Count; i++)
+        {
+            if (InventorySlots[i].ItemInSlot != TItemQuantity.Empty)
+            {
+                tempItemsList.Add(InventorySlots[i].ItemInSlot);
+            }
+        }
+        return tempItemsList;
+    }
+
+    #endregion
 }
